16379198b15e307023d24da416c6e0955fbff437
Taner Öksüz <taner.oeksuez@outlook.com>
2021-05-04 19:01:01 +0200

    testbench added for fpga simulation.

---
+++ vhdl/quartus/testbench.vhd
@@ -0,0 +1,46 @@
+-- args: --std=08 --ieee=synopsys
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use ieee.std_logic_misc.all;
+use std.textio.all;
+
+use work.configure.all;
+use work.constants.all;
+use work.wire.all;
+
+entity testbench is
+end entity testbench;
+
+architecture behavior of testbench is
+
+	component soc
+    port(
+      reset   : in  std_logic;
+      clock   : in  std_logic;
+      uart_rx : in  std_logic;
+      uart_tx : out std_logic
+    );
+	end component;
+
+  signal reset   : std_logic := '0';
+  signal clock   : std_logic := '0';
+
+  signal uart_rx : std_logic := '1';
+  signal uart_tx : std_logic := '1';
+
+begin
+
+	reset <= '1' after 100 ns;
+	clock <= not clock after 20 ns;
+
+	soc_comp : soc
+		port map(
+			reset   => reset,
+			clock   => clock,
+			uart_rx => uart_rx,
+			uart_tx => uart_tx
+		);
+
+end architecture;
