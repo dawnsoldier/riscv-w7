fe7afa515348c41e3bc15c85725ebe7ff89d470a
Taner Öksüz <taner.oeksuez@outlook.com>
2021-05-30 17:03:00 +0200

    block ram reimplemented for diamond.

--- vhdl/diamond/init_memory.py
+++ vhdl/diamond/init_memory.py
@@ -2,29 +2,34 @@
 
 file_in_vhd = open('bram_mem.vhd.init','r')
 file_out_vhd = open('bram_mem.vhd','w')
-file_mem = open('../../sim/work/bram_mem.dat','r')
 
-line_in = file_in_vhd.readline()
-while line_in:
-    file_out_vhd.writelines(line_in)
+for i in range(0,8):
+    file_mem = open('../../sim/work/bram_mem.dat','r')
     line_in = file_in_vhd.readline()
-    if 'signal memory_block : memory_type := (' in line_in:
+    while line_in:
         file_out_vhd.writelines(line_in)
-        break
+        line_in = file_in_vhd.readline()
+        if ('signal memory_block_' + str(i) + ' : memory_type := (') in line_in:
+            file_out_vhd.writelines(line_in)
+            break
 
-line_in = file_mem.readline()
-cnt = 1
-bram_mem_depth = 2**10
-while line_in:
-    if cnt < bram_mem_depth:
-        line_out = "\t\t"+ "x\""+ line_in[0:16] + "\"" + ",\n"
+    cnt = 1
+    cont = 1;
+    bram_mem_depth = 2**10
+    while cont:
+        line_out = "\t\t"
+        for j in range(0,16):
+            line_in = file_mem.readline()
+            line_out = line_out + "x\""+ line_in[14-2*i:16-2*i] + "\""
+            if not line_in or cnt == bram_mem_depth:
+                cont = 0
+                break
+            else:
+                line_out = line_out + ","
+            cnt = cnt + 1
+        line_out = line_out + "\n"
         file_out_vhd.writelines(line_out)
-    else:
-        line_out = "\t\t"+ "x\""+ line_in[0:16] + "\"" + "\n"
-        file_out_vhd.writelines(line_out)
-        break
-    line_in = file_mem.readline()
-    cnt = cnt + 1
+    file_mem.close();
 
 line_in = file_in_vhd.readline()
 while line_in:
@@ -33,4 +38,3 @@ while line_in:
 
 file_in_vhd.close();
 file_out_vhd.close();
-file_mem.close();
